821e7841e3d276a550e2f7a9da86bc4380f963ad
toromuu <diegotoro1998@gmail.com>
2023-05-29 22:40:57 +0200

    [UPDATE] Reduce lambda permissions

--- modules/iam_lambda_role/main.tf
+++ modules/iam_lambda_role/main.tf
@@ -36,29 +36,56 @@ resource "aws_iam_role_policy_attachment" "lambda_basic" {
 # Create a new policy
 resource "aws_iam_policy" "access_policy" {
   name        = "access_policy"
-  description = "Allows access to S3 buckets, ecr, sns"
+  description = "Allows access for ClamAV object parsing and tagging"
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Effect = "Allow"
         Action = [
-          "s3:*",
-          "ecr:*",
-          "lambda:*",
-          "sns:*"
+          "s3:GetObject",
+          "s3:PutObjectTagging",
+          "s3:PutObject",
+          "s3:ListBucket",
+          "s3:GetBucketLocation"
         ]
         Resource = [
-          "*",
+          "arn:aws:s3:::production-clamav-antivirus-scanner-test",
+          "arn:aws:s3:::quarantine-clamav-antivirus-scanner-test",
+          "arn:aws:s3:::production-clamav-antivirus-scanner-test/*",
+          "arn:aws:s3:::quarantine-clamav-antivirus-scanner-test/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:BatchGetImage"
+        ]
+        Resource = [
+          "arn:aws:ecr:::clamav_antivirus_scanner_repository"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "sns:Publish"
+        ]
+        Resource = [
+          "arn:aws:sns:us-east-1:153262203368:emailtoAdmins"
         ]
       }
     ]
   })
 }
 
+
 # //No granuality to avoid recursive dependency cycle
 # //arn:aws:s3:::production-clamav-antivirus-scanner-test
 # //arn:aws:s3:::quarantine-clamav-antivirus-scanner-test
+#          "arn:aws:s3:::${s3_production_name}",
+#          "arn:aws:s3:::${s3_quarantine_name}",
+#          "arn:aws:ecr:::${ecr_repo_name}",
+#          "arn:aws:sns:::${sns_name}"
 
 # Attach new policy to role
 resource "aws_iam_role_policy_attachment" "s3_access" {
